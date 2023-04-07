084a660444f15001c4c2743a8fc7e168e1dcd9fc
tekihei2317 <tekihei2317@gmail.com>
2023-04-07 11:43:53 +0900

    settings.jsonを整理

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,6 +4,12 @@ function pcode() {
   print -z "code $directory -r"
 }
 
+function pcd() {
+  local repository=$(ghq list | peco)
+  local directory="$(ghq root)/$repository"
+  print -z "cd $directory"
+}
+
 function tm() {
   tmux new-session -A -s `basename $PWD`
 }
